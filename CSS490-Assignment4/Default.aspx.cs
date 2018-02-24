aecf3b0fdbfc8267dd4ec134b995bb1f23def041
Peter Stanton <stantp2@uw.edu>
2018-02-23 20:59:53 -0800

    finished load button.

--- CSS490-Assignment4/Default.aspx.cs
+++ CSS490-Assignment4/Default.aspx.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.WindowsAzure.Storage.Blob;
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,6 +15,7 @@ namespace CSS490_Assignment4
 {
     public partial class _Default : Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -25,13 +28,21 @@ namespace CSS490_Assignment4
 
         protected void loadButton_Click(object sender, EventArgs e)
         {
+
+            CloudStorageAccount hiAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting(
+    "css490myblob_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = hiAccount.CreateCloudBlobClient();
+            CloudBlobContainer myCont = blobClient.GetContainerReference("css490peterblobcontainer");
+            CloudBlockBlob myBlob = myCont.GetBlockBlobReference("arrrrghhhhh.txt");
+
+
             WebClient myclient = new WebClient();
             using (myclient)
             {
+
                 Stream inData = myclient.OpenRead("https://css490.blob.core.windows.net/lab4/input.txt");
                 StreamReader myreader = new StreamReader(inData);
-                Uri blobEndpoint = new Uri("https://css490myblob.blob.core.windows.net/");
-                CloudBlockBlob myBlob = new CloudBlockBlob(blobEndpoint);
+                myBlob.UploadFromStream(inData);
             }
         }
     }
