f3f63ab541dc3b3a226ffb5f3fb99ae1c19c209d
Peter Stanton <stantp2@uw.edu>
2018-02-24 21:08:33 -0800

    rewriting to database.

--- CSS490-Assignment4/Default.aspx.cs
+++ CSS490-Assignment4/Default.aspx.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -14,28 +15,6 @@ using System.Web.UI.WebControls;
 
 namespace CSS490_Assignment4
 {
-    public class EmployeeEntity : TableEntity
-    {
-        public string fName { get; set; }
-        public string lName { get; set; }
-        public int id { get; set; }
-        public long phone { get; set; }
-        public string office { get; set; }
-        //define entity here.
-
-        public EmployeeEntity()
-        {
-
-        }
-        public EmployeeEntity(string first, string last, int inId, long inPhone, string inOffice)
-        {
-            fName = first;
-            lName = last;
-            id = inId;
-            phone = inPhone;
-            office = inOffice;
-        }
-    }
     public partial class _Default : Page
     {
 
@@ -64,30 +43,61 @@ namespace CSS490_Assignment4
             CloudTableClient littleBobbyTables = hiAccount.CreateCloudTableClient();
             CloudTable myTable = littleBobbyTables.GetTableReference("Employees");
             WebClient myclient = new WebClient();
+            int rowCounter = 1;
             using (myclient)
             {
                 Stream inData = myclient.OpenRead("https://css490.blob.core.windows.net/lab4/input.txt");
-                StreamReader myreader = new StreamReader(inData);
+                Stream inParse = myclient.OpenRead("https://css490.blob.core.windows.net/lab4/input.txt");
+               // StreamReader myreader = new StreamReader(inData);
+                StreamReader myParse = new StreamReader(inParse);
+                string nowparse = myParse.ReadToEnd();
                 myBlob.UploadFromStream(inData);
+                List<string> hello = new List<string>(Regex.Split(nowparse, Environment.NewLine));
+                hello.RemoveAll(String.IsNullOrWhiteSpace);
 
-                while(!myreader.EndOfStream)
+
+                foreach (var entry in hello)
                 {
-                    String stuff = myreader.ReadLine();
-                    String firstName = "";
-                    String lastName = "";
-                    int Id = -100;
-                    long phone = -999;
-                    string Office = "";
+                    DynamicTableEntity thisOne = new DynamicTableEntity();
+                    Dictionary<string, EntityProperty> data1 = new Dictionary<string, EntityProperty>();
+                    String stuff = entry;
+                    if(entry == "\r") {
+                        break;
+                    }
+                    List<String> parsed = entry.Split(' ').ToList<String>();
+                    parsed.RemoveAll(String.IsNullOrWhiteSpace);
+                    string firstName = parsed[1];
+                    string lastName = parsed[0];
+                    data1.Add("First Name", new EntityProperty(firstName));
+                    data1.Add("Last Name", new EntityProperty(lastName));
+                    foreach (var datum in parsed.Skip(2))
+                    {                    
+                        string[] thisVar = datum.Split('=');
+                        data1.Add(thisVar[0], new EntityProperty(thisVar[1]));
+                    }
+                    thisOne.Properties = data1;
+                    thisOne.PartitionKey = "Partition1";
+                    thisOne.RowKey = Convert.ToString(rowCounter);
+                    var updater = TableOperation.InsertOrReplace(thisOne);
+                    myTable.Execute(updater); 
 
-                    List<String> parsed = stuff.Split(' ').ToList<String>();
-                    firstName = parsed[0];
-                    lastName = parsed[1];
+
+                    /*
                     Id = Convert.ToInt32(parsed[2].Split('=')[1]);
                     phone = Convert.ToInt64(parsed[3].Split('=')[1]);
                     Office = parsed[4].Split('=')[1];
-                    EmployeeEntity thisone = new EmployeeEntity(firstName, lastName, Id, phone, Office);
-                    var myOperation = TableOperation.InsertOrReplace(thisone);
-                    myTable.Execute(myOperation);
+                    DynamicTableEntity thisOne = new DynamicTableEntity();
+                    Dictionary<string, EntityProperty> data1 = new Dictionary<string, EntityProperty>();
+                    data1.Add("First Name", new EntityProperty(firstName));
+                    data1.Add("Last Name", new EntityProperty(lastName));
+                    data1.Add("ID", new EntityProperty(Id));
+                    data1.Add("Phone #", new EntityProperty(phone));
+                    data1.Add("Office location", new EntityProperty(Office));
+                    thisOne.Properties = data1;
+                    thisOne.PartitionKey = "Partition1";
+                    thisOne.RowKey = Convert.ToString(rowCounter);
+                    var updater = TableOperation.InsertOrReplace(thisOne);
+                    myTable.Execute(updater);*/
                 }
             }
         }
